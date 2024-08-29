c7715ce5efe6cc43fac63dd52bbcf20d4b79fbf6
riyuc <bongbang0101@gmail.com>
2024-08-28 22:28:52 -0400

    db migration

---
+++ Makefile
@@ -0,0 +1,1 @@
+# docker run --name ecomm-mysql -p 3307:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql:8.4
---
+++ db/migrations/20240829005801_init_schema.down.sql
@@ -0,0 +1,5 @@
+-- Drop tables if they exist
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products;
+-- Add more DROP TABLE statements as needed
